17fbcf3549540d6628d338046ec86ebd3c93d6b9
Phillip Price <BloaminOnion@users.noreply.github.com>
2021-02-19 11:48:54 -0500

    Add files via upload
    Starting point
    
    This is some already modified Udacity code.  It currently does not work
    and the next steps will be figuring out why.
    
    The end goal for this project is to have a Java program that plays 
    Connect4 either against the user with either two human players or 
    vs an AI opponent.  For now just simple AI opponent will do 
    (random moves), but a future extension might entail using machine
    learning for the AI opponent.

---
+++ Game.java
@@ -0,0 +1,176 @@
+//package com.udacity;
+
+import java.util.Arrays;
+
+/**
+ * Created by me 2021
+ * The Main class containing game logic and backend 2D array
+ */
+public class Game {
+
+    private char turn; // who's turn is it, 'r' or 'y' ? r always starts
+    private boolean twoPlayer; // true if this is a 2 player game, false if AI playing
+    private char [][] grid; // a 2D array of chars representing the game grid
+    private int freeSpots; // counts the number of empty spots remaining on the board (starts from 42 and counts down)
+    private static GameUI gui;
+    private int boardRows = 6; // Y dimensions of grid
+    private int boardColumns = 7; // X dimensions of grid
+    private int [] piecesInColumn = new int[boardColumns]; // Total number of player pieces in each column (max = boardRows)
+
+    /**
+     * Create a new single player game
+     *
+     */
+    public Game() {
+        newGame(false);
+    }
+
+    /**
+     * Create a new game by clearing the 2D grid and restarting the freeSpots counter and setting the turn to x
+     * @param twoPlayer: true if this is a 2 player game, false if playing against the computer
+     */
+    public void newGame(boolean twoPlayer){
+        //sets a game to one or two player
+        this.twoPlayer = twoPlayer;
+
+        // initialize all chars in 6x7 game grid to '-'
+        grid = new char[boardColumns][boardRows];
+        //fill all empty slots with -
+        for (int i=0; i<boardColumns; i++){
+            for (int j=0; j<boardRows; j++){
+                grid[i][j] = '-';
+            }
+        }
+        //start with 42 free spots and decrement by one every time a spot is taken
+        freeSpots = boardRows * boardColumns;
+        //r always starts
+        turn = 'r';
+    }
+
+
+    /**
+     * Gets the char value at that particular position in the grid array
+     * @param i the x index of the 2D array grid
+     * @param j the y index of the 2D array grid
+     * @return the char value at the position (i,j):
+     *          'r' if r has played here
+     *          'y' if y has played here
+     *          '-' if no one has played here
+     *          '!' if i or j is out of bounds
+     */
+    public char gridAt(int i, int j){
+        if(i>=boardColumns||j>=boardRows||i<0||j<0)
+            return '!';
+        return grid[i][j];
+    }
+
+    /**
+     * Places current player's char at position (i,j)
+     * Uses the variable turn to decide what char to use
+     * @param i the x index of the 2D array grid
+     * @param j the y index of the 2D array grid
+     * @return boolean: true if play was successful, false if invalid play
+     */
+    public boolean playAt(int columnToPlay){
+        //check for index boundries
+        if(columnToPlay>=boardColumns||columnToPlay<0)
+            return false;
+        //check if this position is available
+        if(piecesInColumn[columnToPlay]>=boardRows){
+            return false; //bail out if not available
+        }
+        //update grid with new play based on who's turn it is
+        grid[columnToPlay][boardRows - piecesInColumn[columnToPlay]] = turn;
+        //update free spots
+        freeSpots--;
+        //update number of pieces in that column
+        piecesInColumn[columnToPlay]++;
+        return true;
+    }
+
+
+    /**
+     * Override
+     * @return string format for 2D array values
+     */
+    public String toString(){
+        return Arrays.deepToString(this.grid);
+    }
+
+    /**
+     * Performs the winner chack and displayes a message if game is over
+     * @return true if game is over to start a new game
+     */
+    public boolean doChecks() {
+        //check if there's a winner or tie ?
+        String winnerMessage = checkGameWinner(grid);
+        if (!winnerMessage.equals("None")) {
+            gui.gameOver(winnerMessage);
+            newGame(false);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Allows computer to play in a single player game or switch turns for 2 player game
+     */
+    public void nextTurn(){
+        //check if single player game, then let computer play turn
+        if(!twoPlayer){
+            if(freeSpots == 0){
+                return ; //bail out if no more free spots
+            }
+            int aiColumn;
+            do {
+                //randomly pick a position
+                aiColumn = (int) (Math.random() * boardColumns);
+            }while(piecesInColumn[aiColumn]>=boardRows); //keep trying if this spot was taken
+            //update grid with new play, computer is always y
+            grid[aiColumn][boardRows - piecesInColumn[aiColumn]] = 'y';
+            //update free spots
+            freeSpots--;
+            //update number of pieces in that column
+            piecesInColumn[aiColumn]++;
+        }
+        else{
+            //change turns
+            if(turn == 'r'){
+                turn = 'y';
+            }
+            else{
+                turn = 'r';
+            }
+        }
+        return;
+    }
+
+
+    /**
+     * Checks if the game has ended either because a player has won, or if the game has ended as a tie.
+     * If game hasn't ended the return string has to be "None",
+     * If the game ends as tie, the return string has to be "Tie",
+     * If the game ends because there's a winner, it should return "Red wins" or "Yellow wins" accordingly
+     * @param grid 2D array of characters representing the game board
+     * @return String indicating the outcome of the game: "Red wins" or "Yellow wins" or "Tie" or "None"
+     */
+    public String checkGameWinner(char [][]grid){
+        String result = "None";
+        //Student code goes here ...
+        
+        if (freeSpots <= 0) {
+        	result = "All spots filled, start a new game.";
+        }
+        return result;
+    }
+
+    /**
+     * Main function
+     * @param args command line arguments
+     */
+    public static void main(String args[]){
+        Game game = new Game();
+        gui = new GameUI(game);
+    }
+
+}
---
+++ GameUI.java
@@ -0,0 +1,173 @@
+//package com.udacity;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Created by me 2021
+ * The Main UI class containing game logic and backend 2D array
+ */
+public class GameUI extends JPanel {
+
+    //object reference to the actual game to add plays and reflect plays in UI
+    private Game game;
+    //frame to display grid and plays
+    private JFrame frame;
+    //images
+    private BufferedImage grid;
+    private BufferedImage red;
+    private BufferedImage yellow;
+
+    /**
+     * Creates a gameUI given a Game object
+     * @param game
+     */
+    public GameUI(Game game) {
+
+        this.game = game;
+
+        // load images from resources files
+        try {
+            ClassLoader classLoader = GameUI.class.getClassLoader();
+            grid = ImageIO.read(classLoader.getResourceAsStream("GameBoard.png"));
+            red = ImageIO.read(classLoader.getResourceAsStream("RedPiece.png"));
+            yellow = ImageIO.read(classLoader.getResourceAsStream("YellowPiece.png"));
+        } catch (IOException ex) {
+            System.out.println("Failed to load images");
+        }
+
+        //create new game buttons
+        JButton newGameButton = new JButton("New Single Player Game");
+        newGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        newGameButton.addActionListener(new ActionListener() {  // connects the new game button to its buttonPressed method
+            public void actionPerformed(ActionEvent e)
+            {
+                newGameButtonPressed(false);
+            }
+        });
+
+        JButton new2PlayerGameButton = new JButton("New 2 Player Game");
+        new2PlayerGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        new2PlayerGameButton.addActionListener(new ActionListener() {  // connects the new game button to its buttonPressed method
+            public void actionPerformed(ActionEvent e)
+            {
+                newGameButtonPressed(true);
+            }
+        });
+
+        // control what happens when new game buttons cickes
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+                //get grid dimensions and location relative to mouse click
+                JPanel panel = (JPanel)e.getSource();
+                double boardWidth = panel.getWidth();
+                double boardHeight = panel.getHeight();
+                double boardX = 0; // the horizontal gap left of the grid
+                double boardY = 40; // the vertical gap above the grid
+                //return if clicked outside the grid area
+                if(e.getX()<boardX || e.getY() < boardY)
+                    return;
+                //calculate which column of the grid was clicked
+                int i = (int) (7*((e.getX()-boardX)/(boardWidth-boardX)));
+                //take action and update grid 2D array based on where they clicked
+                panelMouseClicked(i);
+            }
+        });
+
+        //add buttons to the panel for display
+        this.add(newGameButton);
+        this.add(new2PlayerGameButton);
+
+        //set panel dimensions
+        final int WIDTH = 700;
+        final int HEIGHT = 640;
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+        //create the frame that would include this panel and siplay it
+        frame = new JFrame();
+        frame.add(this);            // add this panel to the frame
+        frame.pack();               // shrink the window to the appropriate size
+        frame.setResizable(false);  // make the window not resizable
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    // close the application when the window is closed
+        frame.setVisible(true);     // show the window
+
+    }
+
+
+
+
+    @Override
+    public void paint(Graphics g) {
+        //draw everything as normal ..
+        super.paint(g);
+        // .. plus the grid lines ..
+        g.drawImage(grid, 0, 40, null);
+        // .. and loop over the game grid and display all game pieces
+        for(int i=0; i<7; i++){
+            for(int j=0; j<6; j++) {
+                if(game.gridAt(i,j)=='r'){
+                    //based on grid index, calculate the pixel location to draw the red image
+                    g.drawImage(red, 100*i+100,100*j+140, null); // draw a red circle
+                }
+                else if(game.gridAt(i,j)=='y'){
+                    //based on grid index, calculate the pixel location to draw the yellow image
+                    g.drawImage(yellow, 100*i+100,100*j+140, null); // draw a yellow circle
+                }
+            }
+        }
+        
+    }
+
+    /**
+     * Called when mouse is clicked within grid in a valid spot
+     * @param i the x index of the 2D grid array
+     * @param j the y index of the 2D grid array
+     */
+    private void panelMouseClicked(int i) {
+        //apply this play in column i
+        if(!game.playAt(i)){
+            //escape if play was invalid
+            return;
+        }
+        //refresh the display to reflect the new play
+        this.repaint();
+        //check if game ended
+        if(game.doChecks()){
+            //if game over no need to continue, so return
+            return;
+        }
+        //let computer take turn or switch turns for next player
+        game.nextTurn();
+        //refresh the display to reflect computer's turn if played
+        this.repaint();
+        //check if game ended again after computer's turn
+        if(game.doChecks()){
+            //if game over no need to continue, so return
+            return;
+        }
+    }
+
+    public void gameOver(String message){
+        JOptionPane.showMessageDialog(null, message, "Game Over!", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+
+    /**
+     * Called when onw of the new game buttons is clicked
+     * @param twoPlayer boolean: true if starting a 2 player game, false for single player game
+     */
+    private void newGameButtonPressed(boolean twoPlayer) {
+        game.newGame(twoPlayer);
+        this.repaint();
+    }
+
+}
